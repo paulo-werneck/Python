2ae51249ca84e710dc8efbd98dcfd8aa68e43675
paulo-werneck <40461925+paulo-werneck@users.noreply.github.com>
2019-04-05 17:32:00 -0300

    commit connection and aggregation
    commit connection and aggregation

---
+++ agregacoes.py
@@ -0,0 +1,45 @@
+"""
+Paulo Werneck
+03/04/2019
+
+Classe com estatistíscias descritivas
+
+IN: Conexão teradata GPA
+OUT: aggregações em pyspark
+"""
+
+from teradata_gpa.connection import Connection
+from pyspark.sql import SparkSession
+
+
+class Aggregations(Connection):
+
+    def __init__(self):
+        super().__init__()
+
+    def query_fato_fm16(self, cod_loja, dat_apur_venda_inicio, dat_apur_venda_fim, cod_transac_venda):
+        query = 'SELECT * FROM DP_TEDW.FM16_MOV_VEND_CRM ' \
+                'WHERE COD_LOJA = {0} ' \
+                'AND DAT_APURACAO_VENDA BETWEEN {1} AND {2} ' \
+                'AND COD_TRANSAC_VENDA = {3} ' \
+                'ORDER BY NUM_LNH_TRANSAC_VENDA'.format(cod_loja, dat_apur_venda_inicio,
+                                                        dat_apur_venda_fim, cod_transac_venda)
+        return Connection.connection_gpa(self).cursor().execute(query).fetchall()
+
+
+if __name__ == '__main__':
+
+    a = Aggregations()
+    result = a.query_fato_fm16(1, 1181201, 1181231, 8173178654)
+
+    # cria o sparkSession
+    spark = SparkSession \
+        .builder \
+        .appName("Analises descritivas de lojas utilizando pyspark") \
+        .config("spark.some.config.option", "some-value") \
+        .getOrCreate()
+
+    df = spark.createDataFrame(result, )
+    df.printSchema()
+
+    #print(df.show())
---
+++ connection.py
@@ -0,0 +1,38 @@
+"""
+Paulo Werneck
+03/04/2019
+
+Classe para conexão ao teradata
+
+IN: Host, usuario e senha do teradata
+OUT: Conexão estabelecida
+"""
+
+import teradatasql
+from time import sleep
+
+
+class Connection:
+
+    def __init__(self):
+        self.url = '10.100.10.241'
+        self.user = 'TC015369'
+        self.password = 'Cognos04'
+
+    def connection_gpa(self):
+        try:
+            print('Estabelecendo conexão ao teradata . . .')
+            sleep(2)
+            c = teradatasql.connect(None, host=self.url, user=self.user, password=self.password)
+            print('Conexão realizada com sucesso!')
+            exe = 'Seu banco padrão, conectado é: {0} \n'.format(c.cursor().execute('select database').fetchall())
+            print(exe.replace('[', '').replace(']', ''))
+            return c
+        except teradatasql.DatabaseError:
+            print('Falha na conexão ao teradata...')
+
+
+if __name__ == '__main__':
+
+    conn = Connection()
+    conn.connection_gpa()

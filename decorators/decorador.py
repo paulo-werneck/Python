773eb17dca847195a2cb1d9d3f6392d75d293d90
paulo-werneck <40461925+paulo-werneck@users.noreply.github.com>
2020-04-30 17:27:11 -0300

    create decorator.py

---
+++ decorators/decorador.py
@@ -0,0 +1,18 @@
+def replace_special_character(func):
+    def fn_decorada_characters(*args):
+        fnc = func(*args)
+        dict_char = {
+            'a': list('áàâãäå'), 'A': list('ÁÀÂÃÄÅ'),
+            'e': list('éèêẽë'), 'E': list('ÉÈÊẼË'),
+            'i': list('íìîĩï'), 'I': list('ÍÌÎĨÏ'),
+            'o': list('óòôõö'), 'O': list('ÓÒÔÕÖ'),
+            'u': list('úùûũü'), 'U': list('ÚÙÛŨÜ')
+        }
+        if args:
+            text = ' '.join(args)
+        for k, v in dict_char.items():
+            for letter in v:
+                if letter in text:
+                    text = text.replace(letter, k)
+        return func(text)
+    return fn_decorada_characters

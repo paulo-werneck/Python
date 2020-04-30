9bb92df0971d5f814e4753231b7813f07148036b
paulo-werneck <40461925+paulo-werneck@users.noreply.github.com>
2020-04-30 17:36:29 -0300

    Update docstring

--- decorators/decorada.py
+++ decorators/decorada.py
@@ -3,6 +3,12 @@ from cap7.decorador import replace_special_character
 
 @replace_special_character
 def count_characters(*args):
+    """
+        Funcao para contar a quantidade de cada caracteres de uma determinada palavra / frase ou tupla de strings.
+        
+        param1: *args - Parametros variados, aceita varias strings separadas por virgula ou uma tupla de strings desempacotada
+        return: dicionario, sendo chave = caracter a ser contado / valor = a quantidade de vezes que o caracter aparece
+    """
     if args:
         phrase = ' '.join(args)
     l = {i: phrase.count(i) for i in phrase}
